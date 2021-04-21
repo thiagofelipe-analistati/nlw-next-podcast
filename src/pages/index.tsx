0c959130064a2ddda8a354d72ce8ee09e442e78a
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-04-21 20:55:51 -0300

    add do biblioteca axios para as requisições HTTP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 
-// ssr
-// SSG
+import { GetStaticProps } from 'next'
 
 
 
 import React, { useEffect } from "react";
 import { Header } from "../components/Header";
+import { api } from '../services/api';
 
+// cria o obejto epsodio
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+  // ...
 
-export default function Home(props) {
+}
+//cria a lista de obejtos
+type homeProps = {
+   episodes: Episodes[]
+}
+export default function Home(props: homeProps) {
 
 
   return (
@@ -18,7 +29,28 @@ export default function Home(props) {
     </div>
   )
 }
- 
+//ssG -- criação de uma pagina estatica.
+export const  getStaticProps: GetStaticProps = async () =>  {
+  //parametros para definição de quantidade
+  const {data}  = await api.get('episodes',{
+      params: {
+        _limit: 12,
+        _sort: 'published_at',
+        _order: 'desc',
+      }
+  })
+  
+
+  return { 
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*8, //conversão para quando vai ser atualizada. só funciona em prdução.
+  }
+}
+
+ /*
+ ssr --> executa toda vez que alguem acessar a aplicação
 export async function getServerSideProps() {
     const response = await fetch('http://localhost:3333/episodes')
     const data = await response.json()
@@ -29,6 +61,7 @@ export async function getServerSideProps() {
       }
     }
 }
+*/
 
   /*
   // SPA ---> não fica indexável pelo google.
