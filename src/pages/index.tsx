0dbdfe70c9bf989130cc266d0f986252e630ac5a
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-04-21 14:39:14 -0300

    Dependência Json, para uma falsa API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+
+// ssr
+// SSG
+
+
+
+import React, { useEffect } from "react";
 import { Header } from "../components/Header";
 
 
-export default function Home() {
+export default function Home(props) {
+
+
   return (
-
-    <h1>Index</h1>
-
+    <div> 
+     <h1>Index</h1>
+     <p> {JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
  
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return { 
+      props: {
+        episodes: data,
+      }
+    }
+}
+
+  /*
+  // SPA ---> não fica indexável pelo google.
+  useEffect(() =>{
+
+    fetch('http://localhost:3333/episodes')
+      .then (response => response.json())
+      .then(data => console.log(data))
+     
+
+  }, [])
+
+  */
